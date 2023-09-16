2a60d85d85e919d5cce2afa80f0f7b42aa777e75
nazim648 <nazimyar191@gmail.com>
2023-09-16 01:30:20 -0700

    Lacture1

---
+++ src/Layout/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <h1>This is my Footer</h1>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+const Layout = ({children}) => {
+  return (
+    <>
+     <Header />
+     {children}
+     <Footer />
+    </>
+  )
+}
+
+export default Layout
